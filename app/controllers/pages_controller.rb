1de35fcdbf0d0219e3b47712c6465d77e7e49590
Maria Kennedy <mariakingsbury1@gmail.com>
2024-04-29 22:23:28 -0500

    rails project complete

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,13 @@ class PagesController < ApplicationController
     def home
         render :home
     end
+    def about
+        render :about
+    end
+    def contact
+        render :contact
+    end
+    def work
+        render :work
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'home', to: 'pages#home'
   root to: redirect('/home')
+  get 'home', to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  get 'work', to: 'pages#work' 
 end
